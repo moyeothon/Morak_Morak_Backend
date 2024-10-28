7130c9bcfb2ca133ff69a791f6e4d592addd26b9
M1NS1K <rlaalstlr2001@gmail.com>
2024-10-28 22:44:17 +0900

    feat: dependency setting

---
+++ morak/src/main/java/com/morak/morak/MorakApplication.java
@@ -0,0 +1,13 @@
+package com.morak.morak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorakApplication.class, args);
+	}
+
+}
